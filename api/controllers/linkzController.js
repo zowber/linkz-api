f71983cb6afd556956282e3d50444b9f4f24df4c
Andy Bright <andy@zowber.com>
2019-01-07 22:20:16 +0000

    Prettier and get links from label name

--- api/controllers/linkzController.js
+++ api/controllers/linkzController.js
@@ -1,56 +1,81 @@
-'use strict';
+"use strict";
 
-var mongoose = require('mongoose'),
-	Link = mongoose.model('Link');
+var start = process.hrtime();
+
+var elapsed_time = function(note) {
+  var precision = 3; // 3 decimal places
+  var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+  console.log(
+    process.hrtime(start)[0] +
+      " s, " +
+      elapsed.toFixed(precision) +
+      " ms - " +
+      note
+  ); // print message + time
+  start = process.hrtime(); // reset the timer
+};
+
+var mongoose = require("mongoose"),
+  Link = mongoose.model("Link");
 
 exports.list_all_linkz = function(req, res) {
-	console.log('Getting all links');
-	Link.find({}, function (err, link) {
-		if (err)
-			res.send(err);
-		res.json(link);
-	});
+  console.log("Getting all links");
+  Link.find({}, function(err, link) {
+    if (err) res.send(err);
+    res.json(link);
+  });
 };
 
 exports.create_link = function(req, res) {
-	console.log('Creating a new link' );
-	var new_link = new Link(req.body);
-	new_link.save(function(err, link) {
-		if (err)
-			res.send(err);
-		res.json(link);
-	});
+  console.log("Creating a new link");
+  var new_link = new Link(req.body);
+  new_link.save(function(err, link) {
+    if (err) res.send(err);
+    res.json(link);
+  });
 };
 
 exports.read_link = function(req, res) {
-	console.log('Getting ' + req.params.linkId) ;
-	Link.findById(req.params.linkId, function(err, link) {
-		if (err)
-			res.send(err);
-		res.json(link);
-	});
+  console.log("Getting " + req.params.linkId);
+  Link.findById(req.params.linkId, function(err, link) {
+    if (err) res.send(err);
+    res.json(link);
+  });
 };
 
 exports.update_link = function(req, res) {
-	console.log('Updating' + req.params.linkId);
-	Link.findOneAndUpdate({ _id: req.params.linkId }, req.body, {new: true}, function(err, link) {
-		if (err)
-			res.send(err);
-		res.json(link);
-	});
+  console.log("Updating" + req.params.linkId);
+  Link.findOneAndUpdate(
+    { _id: req.params.linkId },
+    req.body,
+    { new: true },
+    function(err, link) {
+      if (err) res.send(err);
+      res.json(link);
+    }
+  );
 };
 
 exports.delete_link = function(req, res) {
-	console.log('Deleteing' + req.params.linkId);
-	Link.remove({
-		_id: req.params.linkId
-	}, function(err, link) {
-		if (err)
-			res.send(err);
-			Link.find({}, function (err, link) {
-				if (err)
-					res.send(err);
-				res.json(link);
-			});
-	});
+  console.log("Deleteing" + req.params.linkId);
+  Link.remove(
+    {
+      _id: req.params.linkId
+    },
+    function(err, link) {
+      if (err) res.send(err);
+      Link.find({}, function(err, link) {
+        if (err) res.send(err);
+        res.json(link);
+      });
+    }
+  );
+};
+
+exports.get_links_by_label = function(req, res) {
+  console.log("Getting linkz by label " + req.params.name);
+  Link.find({ "labels.name": req.params.name }, function(err, link) {
+    if (err) res.send(err);
+    res.json(link);
+  });
 };
--- api/routes/linkzRoutes.js
+++ api/routes/linkzRoutes.js
@@ -1,14 +1,18 @@
-'use strict';
+"use strict";
 
 module.exports = function(app) {
-	var linkz = require('../controllers/linkzController');
+  var linkz = require("../controllers/linkzController");
 
-	app.route('/linkz')
-		.get(linkz.list_all_linkz)
-		.post(linkz.create_link);
+  app
+    .route("/linkz")
+    .get(linkz.list_all_linkz)
+    .post(linkz.create_link);
 
-	app.route('/linkz/:linkId')
-		.get(linkz.read_link)
-		.put(linkz.update_link)
-		.delete(linkz.delete_link);
+  app
+    .route("/linkz/:linkId")
+    .get(linkz.read_link)
+    .put(linkz.update_link)
+    .delete(linkz.delete_link);
+
+  app.route("/linkz/getLinkzByLabel/:name").get(linkz.get_links_by_label);
 };
